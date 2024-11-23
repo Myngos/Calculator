bedae43528afe7fe71205269a70e195c4b8ac3f6
Mingos <moniusdomingo065@gmail.com>
2024-11-23 14:35:30 +0200

    start

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.querySelector('.container');
+
+function add(a,b){
+    return a+b
+}
+
+function subtruct(a,b){
+    return a - b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+let num1, num2, operator;
+
+function operate(num1, num2, operator){
+    return add(num1,num2)
+}
